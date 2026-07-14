b6b59bf59eaad7e2df4a5f9fc4dcb106d094d4a8
jojechta1-alt <jojechta1@gmail.com>
2026-07-14 12:03:22 -0500

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,1 +1,9 @@
-
+export default function handler(req, res) {
+  res.setHeader('Content-Type', 'application/javascript');
+  res.status(200).send(`
+    window.env = {
+      SUPABASE_URL: "${process.env.SUPABASE_URL || ''}",
+      SUPABASE_KEY: "${process.env.SUPABASE_KEY || ''}"
+    };
+  `);
+}
